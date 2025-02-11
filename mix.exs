c166b3ed73add76f4596a7e939a4cc804da96070
Jethro Cao <jethrocao@gmail.com>
2025-02-10 22:17:32 -0600

    Decrement elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule App.MixProject do
       app: :bittorrent,
       version: "1.0.0",
       escript: [main_module: Bittorrent.CLI],
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
     ]

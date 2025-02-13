02cbc6c824008b698fe27c85f248027a1aa50dc9
Jethro Cao <jethrocao@gmail.com>
2025-02-12 21:00:44 -0600

    Move metainfo extraction to own module

--- lib/bencode.ex
+++ lib/bencode.ex
@@ -1,38 +1,4 @@
 defmodule Bencode do
-    def get_info_hash(encoded_value) when is_binary(encoded_value) do
-        :binary.bin_to_list(encoded_value)
-        |> find_info_start()
-        |> find_info_content(0, [])
-        |> then(&(:crypto.hash(:sha, &1)))
-        |> Base.encode16(case: :lower)
-    end
-    def get_info_hash(_), do: "Invalid encoded value: not binary"
-
-    defp find_info_start(~c"4:info" ++ rest), do: rest
-    defp find_info_start([_ | rest]), do: find_info_start(rest)
-
-    defp find_info_content([?e | _rest], 1, res), do: Enum.reverse([?e | res])
-    defp find_info_content([?e | rest], lvl, res), do: find_info_content(rest, lvl-1, [?e | res])
-    defp find_info_content([?: | rest], lvl, res) do
-        strlen = Enum.find_index(res, &(&1 not in ~c"0123456789"))
-            |> then(&(Enum.slice(res, 0, &1)))
-            |> Enum.reverse()
-            |> List.to_integer()
-        {wrev, rest} = {Enum.slice(rest, 0, strlen) |> Enum.reverse(), Enum.slice(rest, strlen..-1//1)}
-        find_info_content(rest, lvl, wrev ++ [?: | res])
-    end
-    defp find_info_content([init | rest], lvl, res) when init in [?i, ?l, ?d], do: find_info_content(rest, lvl+1, [init | res])
-    defp find_info_content([char | rest], lvl, res), do: find_info_content(rest, lvl, [char | res])
-
-    def get_encoded_pieces_bytels(encoded_value) when is_binary(encoded_value) do
-        pieces_start = :binary.bin_to_list(encoded_value) |> find_pieces_start()
-        colon_idx = Enum.find_index(pieces_start, &(&1===?:))
-        pieces_enc_len = Enum.slice(pieces_start, 0, colon_idx) |> List.to_integer()
-        Enum.slice(pieces_start, colon_idx+1, pieces_enc_len)
-    end
-    defp find_pieces_start(~c"6:pieces" ++ rest), do: rest
-    defp find_pieces_start([_ | rest]), do: find_pieces_start(rest)
-
     def decode(encoded_value) when is_binary(encoded_value) do
         [{_type, item}] = parse(:binary.bin_to_list(encoded_value), [])
         item
